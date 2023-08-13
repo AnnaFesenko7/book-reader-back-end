3642d3fb61e5272a0484f76dfd07e5e665a44e15
AnnaFesenko7 <annafecenko7@gmail.com>
2023-08-13 15:30:32 +0300

    fix

--- src/controllers/auth/googleRedirect.js
+++ src/controllers/auth/googleRedirect.js
@@ -42,7 +42,7 @@ const googleRedirect = async (req, res) => {
   await User.findByIdAndUpdate(profile._id, { token });
 
   return res.redirect(
-    `${process.env.FRONTEND_URL}login/?token=${token}&name=${name}&avatar=${picture}`
+    `${process.env.FRONTEND_URL}/login/?token=${token}&name=${name}&avatar=${picture}`
   );
   // return res.redirect(
   //   `${process.env.FRONTEND_URL}?email=${userData.data.email}`
